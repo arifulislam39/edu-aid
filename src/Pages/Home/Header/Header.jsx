0fc30b4f4d4827d724a06567dd47a43a8865b9a1
ariful-islam <arifulislam204909@gmail.com>
2023-07-22 22:17:06 +0600

    navbar and footer added

---
+++ src/Pages/Home/Header/Header.jsx
@@ -0,0 +1,10 @@
+import img from "../../../assets/banner.jpg"
+const Header = () => {
+    return (
+        <div>
+           <img className="w-full h-[700px]" src={img} alt="" />
+        </div>
+    );
+};
+
+export default Header;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import CollegeCards from "../CollegeCards/CollegeCards";
 import CollegeGallery from "../CollegeGallery/CollegeGallery";
+import Header from "../Header/Header";
 import ResearchPaper from "../ResearchPaper.jsx/ResearchPaper";
 import Reviews from "../Reviews/Reviews";
 
 const Home = () => {
     return (
         <div>
+            <Header></Header>
             <CollegeCards></CollegeCards>
             <CollegeGallery></CollegeGallery>
             <ResearchPaper></ResearchPaper>
